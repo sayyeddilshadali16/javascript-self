fa5717710110a8b20375a85755a0130a23f60b31
Sayyed Dilshad Ali <sayyeddilshad26@gmail.com>
2024-06-15 06:38:16 +0530

    index

---
+++ index.js
@@ -0,0 +1,22 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const evenNumbers = numbers.filter((num) => {
+//   return num % 2 === 0;
+// });
+
+// console.log(evenNumbers);
+
+// const sumOfArrary = numbers.reduce((acc, curr) => {
+//   return acc + curr;
+// });
+// console.log(sumOfArrary);
+
+// const square = numbers.map((num)=>{
+//     return num**2
+// })
+// console.log(square)
+
+// const filterNum = numbers.filter((num)=>{
+//     return num>=10
+// })
+// console.log(filterNum)
